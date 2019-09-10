ec474f1f64d117583295f98e0960199464d5b329
lizw <lzw1000000@163.com>
2019-09-10 20:44:32 +0800

    优化

--- src/pages/workbench/index.js
+++ src/pages/workbench/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import 'font-awesome/css/font-awesome.min.css';
 // import lodash from 'lodash';
 // import $ from 'jquery';
 import { leftCenterLayout } from './layout-split';
