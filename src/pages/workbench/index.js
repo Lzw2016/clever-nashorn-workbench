a6c7971dd98da3d429469cbc317eed4970d7ab31
lizw <lzw1000000@163.com>
2019-09-11 15:15:23 +0800

    优化

--- src/pages/workbench/index.js
+++ src/pages/workbench/index.js
@@ -1,10 +1,17 @@
 import './index.scss';
 import 'font-awesome/css/font-awesome.min.css';
 // import lodash from 'lodash';
-// import $ from 'jquery';
+import $ from 'jquery';
 import { leftCenterLayout } from './layout-split';
 import { vConsole } from './vconsole';
 import { editorInstance } from './monaco-editor';
+
+$(document).ready(() => {
+  // 显示内容
+  $(".workbench-root").css("visibility", "visible");
+  // 禁止鼠标右键菜单
+  document.oncontextmenu = () => false;
+});
 
 export {
   leftCenterLayout,
