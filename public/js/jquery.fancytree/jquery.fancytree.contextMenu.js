57704d3df47c352b9679d19a8cc66dcff2e3ac42
lizw <lzw1000000@163.com>
2019-09-18 09:34:33 +0800

    优化

--- public/js/jquery.fancytree/jquery.fancytree.contextMenu.js
+++ public/js/jquery.fancytree/jquery.fancytree.contextMenu.js
@@ -8,78 +8,81 @@
  * Released under the MIT license
  * https://github.com/mar10/fancytree/wiki/LicenseInfo
  */
-(function($, document) {
-	"use strict";
+(function ($, document) {
+  "use strict";
 
-	var initContextMenu = function(tree, selector, menu, actions) {
-		tree.$container.on("mousedown.contextMenu", function(event) {
-			var node = $.ui.fancytree.getNode(event);
+  var initContextMenu = function (tree, selector, menu, actions) {
+    tree.$container.on("mousedown.contextMenu", function (event) {
+      var node = $.ui.fancytree.getNode(event);
 
-			if (node) {
-				$.contextMenu("destroy", "." + selector);
+      if (node) {
+        $.contextMenu("destroy", "." + selector);
 
-				node.setFocus(true);
-				node.setActive(true);
+        node.setFocus(true);
+        node.setActive(true);
 
-				$.contextMenu({
-					selector: "." + selector,
-					events: {
-						show: function(options) {
-							options.prevKeyboard = tree.options.keyboard;
-							tree.options.keyboard = false;
-						},
-						hide: function(options) {
-							tree.options.keyboard = options.prevKeyboard;
-							node.setFocus(true);
-						},
-					},
-					build: function($trigger, e) {
-						node = $.ui.fancytree.getNode($trigger);
+        $.contextMenu({
+          selector: "." + selector,
+          events: {
+            show: function (options) {
+              options.prevKeyboard = tree.options.keyboard;
+              tree.options.keyboard = false;
+            },
+            hide: function (options) {
+              tree.options.keyboard = options.prevKeyboard;
+              node.setFocus(true);
+            },
+          },
+          build: function ($trigger, e) {
+            node = $.ui.fancytree.getNode($trigger);
+            if (node) {
+              node.setFocus(true);
+              node.setActive(true);
+            }
+            var menuItems = {};
+            if ($.isFunction(menu)) {
+              menuItems = menu(node);
+            } else if ($.isPlainObject(menu)) {
+              menuItems = menu;
+            }
 
-						var menuItems = {};
-						if ($.isFunction(menu)) {
-							menuItems = menu(node);
-						} else if ($.isPlainObject(menu)) {
-							menuItems = menu;
-						}
+            return {
+              callback: function (action, options) {
+                if ($.isFunction(actions)) {
+                  actions(node, action, options);
+                } else if ($.isPlainObject(actions)) {
+                  if (
+                    actions.hasOwnProperty(action) &&
+                    $.isFunction(actions[action])
+                  ) {
+                    actions[action](node, options);
+                  }
+                }
+              },
+              items: menuItems,
+            };
+          },
+        });
+      }
+    });
+  };
 
-						return {
-							callback: function(action, options) {
-								if ($.isFunction(actions)) {
-									actions(node, action, options);
-								} else if ($.isPlainObject(actions)) {
-									if (
-										actions.hasOwnProperty(action) &&
-										$.isFunction(actions[action])
-									) {
-										actions[action](node, options);
-									}
-								}
-							},
-							items: menuItems,
-						};
-					},
-				});
-			}
-		});
-	};
-
-	$.ui.fancytree.registerExtension({
-		name: "contextMenu",
-		version: "@VERSION",
-		contextMenu: {
-			selector: "fancytree-title",
-			menu: {},
-			actions: {},
-		},
-		treeInit: function(ctx) {
-			this._superApply(arguments);
-			initContextMenu(
-				ctx.tree,
-				ctx.options.contextMenu.selector || "fancytree-title",
-				ctx.options.contextMenu.menu,
-				ctx.options.contextMenu.actions
-			);
-		},
-	});
+  $.ui.fancytree.registerExtension({
+    name: "contextMenu",
+    version: "@VERSION",
+    contextMenu: {
+      selector: "fancytree-title",
+      menu: {},
+      actions: {},
+    },
+    treeInit: function (ctx) {
+      this._superApply(arguments);
+      initContextMenu(
+        ctx.tree,
+        ctx.options.contextMenu.selector || "fancytree-title",
+        ctx.options.contextMenu.menu,
+        ctx.options.contextMenu.actions
+      );
+    },
+  });
 })(jQuery, document);
