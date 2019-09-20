09731c371a4cfa6d17370f65da1acaa62d239792
lizw <lzw1000000@163.com>
2019-09-20 09:24:11 +0800

    ali oss

---
+++ ali-oss-conf.js
@@ -0,0 +1,11 @@
+const aliOssConf = {
+  region: 'oss-cn-hangzhou',
+  accessKeyId: '',
+  accessKeySecret: '',
+  bucket: 'clever-nashorn',
+  ossUrl: 'https://clever-nashorn.oss-cn-hangzhou.aliyuncs.com',
+};
+
+module.exports = {
+  ...aliOssConf,
+};
